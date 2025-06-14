c8b09a21f949b47ab576c6b0aa3ec36a0eaf5f6d
Jethro Djan <jthryeboah@gmail.com>
2025-06-14 09:48:52 +0000

    Add other test options -- tex-style commands with and without options

--- src/lib.rs
+++ src/lib.rs
@@ -20,14 +20,46 @@ mod tests {
     #[test]
     fn parse_ctx_style_command() {
         assert_eq!(
-            parser::ctx_command("\\externalfigure[cow.pdf][scale=300]"), 
+            parser::ctx_command(r"\externalfigure[cow.pdf][scale=300]"), 
             Ok((
-                "\\externalfigure[cow.pdf][scale=300]", 
+                "", 
                 Node::Command(Command {
                     name: "externalfigure".to_string(),
                     style: CommandStyle::ContextStyle,
                     options: vec!["scale=300".to_string()],
                     arguments: vec![Node::Text("cow.pdf".to_string())],
+                })
+            ))
+        );
+    }
+
+    #[test]
+    fn parse_tex_style_command() {
+        assert_eq!(
+            parser::ctx_command(r"\framed[width=textwidth]{Hi}"), 
+            Ok((
+                "", 
+                Node::Command(Command {
+                    name: "framed".to_string(),
+                    style: CommandStyle::TexStyle,
+                    options: vec!["width=textwidth".to_string()],
+                    arguments: vec![Node::Text("Hi".to_string())],
+                })
+            ))
+        );
+    }
+
+    #[test]
+    fn parse_tex_style_command_no_options() {
+        assert_eq!(
+            parser::ctx_command(r"\emph{Hey}"), 
+            Ok((
+                "", 
+                Node::Command(Command {
+                    name: "emph".to_string(),
+                    style: CommandStyle::TexStyle,
+                    options: Vec::new(),
+                    arguments: vec![Node::Text("Hey".to_string())],
                 })
             ))
         );
--- src/parser.rs
+++ src/parser.rs
@@ -2,8 +2,8 @@ use nom::{
     bytes::complete::{tag, take_until}, 
     character::complete::{not_line_ending, alpha1},
     combinator::{opt, map_res, map},
-    sequence::{preceded, tuple, delimited}, 
-    multi::{many0, many1},
+    sequence::{preceded, delimited, separated_pair, tuple}, 
+    multi::{many0, many1, separated_list1},
     IResult, 
     Parser,
     branch::alt,
@@ -15,28 +15,61 @@ pub fn ctx_command(input: &str) -> IResult<&str, Node> {
     let (input, _) = tag("\\")(input)?;
     let (input, command_name) = alpha1(input)?;
 
+    // Case 4: ConTeXt-style command [arg][opt]
     if let Ok((remaining, (arg, opt))) = parse_context_style_args(input) {
         return Ok((remaining, Node::Command(Command {
-            name: command_name.to_string(), 
-            style: CommandStyle::TexStyle,
-            options: opt.unwrap_or_default(), 
-            arguments: vec![Node::Text(arg)], 
+            name: command_name.to_string(),
+            style: CommandStyle::ContextStyle,
+            options: opt.unwrap_or_default(),
+            arguments: vec![Node::Text(arg.to_string())],
         })));
     }
 
-    let (input, opt) = opt(parse_command_options).parse(input)?;
-    let (input, args) = match parse_group(input) {
-        Ok((i, a)) => (i, a),
-        Err(_) => (input, Vec::new()), 
+    // Cases 1-3: TeX-style commands
+    let (input, maybe_options) = opt(parse_command_options).parse(input)?;
+    let (input, maybe_args) = opt(parse_group).parse(input)?;
+
+    // If we found args but no options, try looking for options after args
+    let (input, options) = if maybe_args.is_some() && maybe_options.is_none() {
+        opt(parse_command_options).parse(input)?
+    } else {
+        (input, maybe_options)
     };
 
     Ok((input, Node::Command(Command {
-        name: command_name.to_string(), 
+        name: command_name.to_string(),
         style: CommandStyle::TexStyle,
-        options: opt.unwrap_or_default(), 
-        arguments: args, 
+        options: options.unwrap_or_default(),
+        arguments: maybe_args.unwrap_or_default(),
     })))
 }
+
+// pub fn ctx_command(input: &str) -> IResult<&str, Node> {
+//     let (input, _) = tag("\\")(input)?;
+//     let (input, command_name) = alpha1(input)?;
+// 
+//     if let Ok((remaining, (arg, opt))) = parse_context_style_args(input) {
+//         return Ok((remaining, Node::Command(Command {
+//             name: command_name.to_string(), 
+//             style: CommandStyle::ContextStyle,
+//             options: opt.unwrap_or_default(), 
+//             arguments: vec![Node::Text(arg)], 
+//         })));
+//     }
+// 
+//     let (input, opt) = opt(parse_command_options).parse(input)?;
+//     let (input, args) = match parse_group(input) {
+//         Ok((i, a)) => (i, a),
+//         Err(_) => (input, Vec::new()), 
+//     };
+// 
+//     Ok((input, Node::Command(Command {
+//         name: command_name.to_string(), 
+//         style: CommandStyle::TexStyle,
+//         options: opt.unwrap_or_default(), 
+//         arguments: args, 
+//     })))
+// }
 
 pub fn ctx_text(input: &str) -> IResult<&str, Node> {
     let (input, text) = take_until("\\")(input)?;
@@ -60,35 +93,49 @@ pub fn ctx_code(input: &str) -> IResult<&str, Vec<Node>> {
     ))).parse(input)
 }
 
-pub fn parse_context_style_args(input: &str) -> IResult<&str, (String, Option<Vec<String>>)> {
+fn parse_context_style_args(input: &str) -> IResult<&str, (&str, Option<Vec<String>>)> {
     tuple((
-        map(
-            delimited(tag("["), take_until("]"), tag("]")),
-            |s: &str| s.to_string()
-        ),
+        delimited(tag("["), take_until("]"), tag("]")),
         opt(parse_command_options),
     )).parse(input)
 }
 
+// pub fn parse_context_style_args(input: &str) -> IResult<&str, (String, Option<Vec<String>>)> {
+//     let (input, arg) = delimited(tag("["), take_until("]"), tag("]")).parse(input)?;
+//     let (input, options) = opt(parse_command_options).parse(input)?;
+//     
+//     Ok((input, (arg.to_string(), options)))
+// }
+
+// pub fn parse_context_style_args(input: &str) -> IResult<&str, (String, Option<Vec<String>>)> {
+//     let (input, arg) = delimited(tag("["), take_until("]"), tag("]")).parse(input)?;
+//     let (input, options) = opt(parse_command_options).parse(input)?;
+//     
+//     Ok((input, (arg.to_string(), options)))
+// }
+
 pub fn parse_command_options(input: &str) -> IResult<&str, Vec<String>> {
     delimited(
         tag("["),
-        many1(map(
-            take_until("]"),
-            |s: &str| s.to_string()
-        )),
+        separated_list1(
+            tag("]["),
+            map(
+                take_until("]"),
+                |s: &str| s.to_string()
+            )
+        ),
         tag("]"),
     ).parse(input)
 }
 
 pub fn parse_group(input: &str) -> IResult<&str, Vec<Node>> {
     delimited(
-        tag("["),
+        tag("{"),
         many1(alt((
             ctx_command,
             ctx_comment,
             ctx_text,
         ))),
-        tag("]"),
+        tag("}"),
     ).parse(input)
 }
